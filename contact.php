37dc61c647ca00d9e5393d81778bffab7c5a2f21
amigoster <amigosteradmin@gmail.com>
2014-04-16 20:55:38 -0700

    New images and contact email

--- contact.php
+++ contact.php
@@ -28,7 +28,7 @@ if($_POST) {
 	//Attempt to send email
 	if ($errors === 0) {
 		$emailfrom = "=?UTF-8?B?" . base64_encode ( 'hell0World.org (Mentor Application)' ) . "?=";
-		$emailto = "recruiterlinh@gmail.com";
+		$emailto = "hell0worldgang@gmail.com";
 		$subject = "hell0World.org (Mentor Application)";
 		
 		$headers = 'From: ' . $emailfrom . ' <contact@hell0world.org>' . "\r\n" . 'Reply-To: ' . $email . "\r\n" . 'X-Mailer: PHP/' . phpversion ();
